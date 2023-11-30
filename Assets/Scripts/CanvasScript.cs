abbcbf22aa1d4127de7e1138766d160372b25789
Kurosaki-Yuki <visholol2@gmail.com>
2023-11-30 09:26:15 -0300

    viento

--- Assets/Scripts/CanvasScript.cs
+++ Assets/Scripts/CanvasScript.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class CanvasScript : MonoBehaviour
 {
     public LaunchScript launchScript;
+    public WindBehavior windBehavior;
 
     public TextMeshProUGUI vector;
     public TextMeshProUGUI speed;
     public TextMeshProUGUI mass;
-    public TextMeshProUGUI score;
+    public TextMeshProUGUI wind;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,6 @@ public class CanvasScript : MonoBehaviour
         vector.text = "Vector de lanzamiento: (" + launchScript.getVSpeed().x + ", " + launchScript.getVSpeed().y + ", " + launchScript.getVSpeed().z + ")";
         speed.text = "Fuerza de lanzamiento: " + launchScript.speed.ToString("0");
         mass.text = "Masa del projectil: " + launchScript.mass.ToString("0");
-        score.text = "Puntaje: ";
+        wind.text = "Velocidad del viento: " + windBehavior.windForceMagnitude.ToString("0"); ;
     }
 }
--- Assets/Scripts/WindBehavior.cs
+++ Assets/Scripts/WindBehavior.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class WindBehavior : UtalGameObject
 {
-    public float windForceMagnitude = 10;
+    public float windForceMagnitude = 0;
+
+    private float actTime;
+    private float maxTime = 5;
+
+    private void Update()
+    {
+        if (actTime >= maxTime)
+        {
+            windForceMagnitude = Random.Range(-100, 100);
+            actTime = 0;
+        }
+
+        actTime += Time.deltaTime;
+    }
     public override void UtalOnTriggerStay(UtalCollider collider)
     {
         //Debug.Log(collider.utalRigidbody.gameObject.name);
