1074739ec5e05e399710f444fa1d648fd7e6a1a8
GermanAliaga <usami.haruka01@gmail.com>
2023-11-30 21:44:00 -0300

    ?

--- Assets/Scripts/PointManager.cs
+++ Assets/Scripts/PointManager.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 public class PointManager : MonoBehaviour
 {
     private int points = 0;
+    public UtalSphereCollider bullet;
+    public UtalSphereCollider nodo1;
+    public UtalSphereCollider nodo2;
+    public UtalSphereCollider nodo3;
+
+    public Node nodito1;
+    public Node nodito2;
+    public Node nodito3;
 
     public void Update()
     {
-       // Debug.Log(points);
+        if(nodo1.UtalCheckCollision(bullet))
+        {
+            addPoints(nodito1.score);
+        }
+        if (nodo2.UtalCheckCollision(bullet))
+        {
+            addPoints(nodito2.score);
+        }
+        if (nodo3.UtalCheckCollision(bullet))
+        {
+            addPoints(nodito3.score);
+        }
     }
-
 
     public void addPoints(int p)
     {
